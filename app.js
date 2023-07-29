718c6d9eb0fceda9d1f0c1dfa09fadb132300f7b
Terri Fricker <51532684+terrifricker@users.noreply.github.com>
2023-07-29 15:54:09 -0700

    rewrite javascript to just toggle active class

--- app.js
+++ app.js
@@ -1,17 +1,7 @@
-const showIcons = document.querySelectorAll(".show-icon");
-const closeIcons = document.querySelectorAll(".close-icon");
+const buttons = document.querySelectorAll("button");
 
-showIcons.forEach(icon => icon.addEventListener("click", (event) => {
-    event.target.parentNode.classList.add("hidden");
-    event.target.parentNode.parentNode.childNodes[7].classList.remove("hidden");
-    event.target.parentNode.parentNode.childNodes[5].classList.remove("hidden");
-    }
+
+buttons.forEach(button => button.addEventListener("click", (event) =>
+    event.target.parentNode.parentNode.classList.toggle("active")
 ));
 
-closeIcons.forEach(icon => icon.addEventListener("click", (event) => {
-    console.log(event.target.parentNode.parentNode.childNodes)
-    event.target.parentNode.classList.add("hidden");
-    event.target.parentNode.parentNode.childNodes[7].classList.add("hidden");
-    event.target.parentNode.parentNode.childNodes[3].classList.remove("hidden");
-    }
-))
