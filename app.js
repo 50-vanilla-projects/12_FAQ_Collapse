8a15985d3b721f278334130edb7d1db3f5abe7fd
Terri Fricker <51532684+terrifricker@users.noreply.github.com>
2023-07-29 12:25:50 -0700

    added another parentNode to event because the icon is inside button

--- app.js
+++ app.js
@@ -2,15 +2,16 @@ const showIcons = document.querySelectorAll(".show-icon");
 const closeIcons = document.querySelectorAll(".close-icon");
 
 showIcons.forEach(icon => icon.addEventListener("click", (event) => {
-    event.target.classList.add("hidden");
-    event.target.parentNode.childNodes[7].classList.remove("hidden");
-    event.target.parentNode.childNodes[5].classList.remove("hidden");
+    event.target.parentNode.classList.add("hidden");
+    event.target.parentNode.parentNode.childNodes[7].classList.remove("hidden");
+    event.target.parentNode.parentNode.childNodes[5].classList.remove("hidden");
     }
 ));
 
 closeIcons.forEach(icon => icon.addEventListener("click", (event) => {
-    event.target.classList.add("hidden");
-    event.target.parentNode.childNodes[7].classList.add("hidden");
-    event.target.parentNode.childNodes[3].classList.remove("hidden");
+    console.log(event.target.parentNode.parentNode.childNodes)
+    event.target.parentNode.classList.add("hidden");
+    event.target.parentNode.parentNode.childNodes[7].classList.add("hidden");
+    event.target.parentNode.parentNode.childNodes[3].classList.remove("hidden");
     }
 ))
